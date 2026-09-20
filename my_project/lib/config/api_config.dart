56f3ad0fa440a11769b9fe73aa6e1782b507fad2
Muhammad Adeel <itsadeel9015@gmail.com>
2026-09-20 01:43:29 -0700

    Connect Flutter app to production backend

--- my_project/lib/config/api_config.dart
+++ my_project/lib/config/api_config.dart
@@ -1,11 +1,3 @@
-import 'package:flutter/foundation.dart';
-
 class ApiConfig {
-  static const String pcIp = '192.168.1.6';
-  static const int port = 3000;
-
-  static String get baseUrl {
-    final host = kIsWeb ? Uri.base.host : pcIp;
-    return 'http://$host:$port';
-  }
+  static const String baseUrl = 'https://invoice-management-sysytem.vercel.app';
 }
